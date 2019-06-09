a7fc68f47f13fa95104614a0f12536f6e584cd7a
Abdul <awsayed@alaska.edu>
2019-06-09 00:06:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,19 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.create
+    #Initialize a song object and save it to @@all through Song.all
+    self.all << self.new
   end
 
+  def save
+    #Places a Song instance object into @@all
+    self.class.all << self
+  end
 end
